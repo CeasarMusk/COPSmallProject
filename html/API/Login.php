474335472492f3e573a2a2df645019c744e04beb
Abhinav-Pollepalli <pollepallia@gmail.com>
2025-09-21 19:46:08 -0400

    Update Login.php

--- html/API/Login.php
+++ html/API/Login.php
@@ -9,7 +9,7 @@
 
     $conn = new mysqli("localhost", "API", "!Password123b", "COP4331");
     
-    //Cmmd to avoid user to plugin strange characters
+    
     $conn->set_charset("utf8mb4");
 
 	if( $conn->connect_error )
@@ -60,3 +60,4 @@
 	}
 	
 ?>
+
